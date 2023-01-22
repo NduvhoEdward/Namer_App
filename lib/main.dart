327ba1d8bc02acf9e1b630f998f99158b7a18841
NduvhoEdward <Nduvho.Ramashia@gmail.com>
2023-01-22 19:43:35 +0200

    Added the option/ability to remove favourates

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,11 @@ class MyAppState extends ChangeNotifier {
     }
     notifyListeners();
   }
+
+  void removeFav(selectedPair) {
+    favorites.remove(selectedPair);
+    notifyListeners();
+  }
 }
 
 // ...
@@ -64,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -152,7 +157,73 @@ class GeneratorPage extends StatelessWidget {
   }
 }
 
+/*
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //ListView(
+          //  children: [
+          Text("I'm dedicating every day to you"),
+          Text('Domestic life was never quite my style'),
+          Text('When you smile, you knock me out, I fall apart'),
+          Text('And I thought I was so smart'),
+          //  ],
+          //  )
+        ],
+      ),
+    );
+  }
+}
+*/
+
 // ...
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            title: ElevatedButton.icon(
+              onPressed: () {
+                appState.removeFav(pair);
+              },
+              icon: Icon(Icons.delete),
+              label: Text(pair.asLowerCase),
+            ),
+          ),
+      ],
+    );
+  }
+}
 
 class BigCard extends StatelessWidget {
   const BigCard({
